8e01e0691f1ed0d3a40062a11d0e32fe0f6f5f8c
rsandoval1 <r.sandovaljr1@gmail.com>
2025-10-05 21:48:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,31 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
-
 );
 
-  serviceWorkerRegistration.register({
-  onUpdate: registration => {
-    if (window.confirm("A new version is available. Reload now?")) {
-      if (registration && registration.waiting) {
-        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-        registration.waiting.addEventListener('statechange', e => {
-          if (e.target.state === 'activated') {
-            window.location.reload();
-          }
-        });
-      }
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+    
+    if (waitingServiceWorker) {
+      console.log('New service worker waiting to activate');
+      
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+      waitingServiceWorker.addEventListener('statechange', (e) => {
+        if (e.target.state === 'activated') {
+          window.location.reload();
+        }
+      });
+
     }
+  },
+  onSuccess: (registration) => {
+    console.log('Service worker registered successfully');
+  },
+  onError: (error) => {
+    console.error('Service worker registration failed:', error);
   }
 });
